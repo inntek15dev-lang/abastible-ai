92818f4207037a3db7081efaf52412c8bbecf97d
pablosolisdalencon <inntek1.5dev@gmail.com>
2026-03-02 17:47:00 -0300

    up seeders programa envasado

---
+++ back/src/data/envasado.js
@@ -0,0 +1,165 @@
+const envasadoProgramData = [
+    {
+        "numero": "1",
+        "nombre": "Liderazgo y compromiso",
+        "actividades": [
+            {
+                "codigo": "1.1",
+                "descripcion": "Todo contacto de seguridad deberá ser planificado, según las directrices de programa safealign, y esta planificación tendrá su foco en los análisis de tendencia de accidentes. Se deberá realizar los CS, SST e IRF acorde a las metas establecidas en el programa Safealign, y de acuerdo al número de personas que deban reportar.\\nContactos de Seguridad (CS): 2 CS por cada jefatura o línea de supervisión que se hayan asignado.\\nSesiones de Seguridad en el trabajo (SST): 2 SST por cada jefatura o línea de supervisión que se hayan asignado.\\nInspección de Riesgos Físicos (IRF): 2 IRF por cada jefatura o línea de supervisión que se hayan asignado.",
+                "frecuencia": "mensual",
+                "criterios": "Registro +Seguridad",
+                "requiere_evidencia": 1
+            },
+            {
+                "codigo": "1.2",
+                "descripcion": "La actividad corresponde a la participación activa en las reuniones y comisiones del CPHS de faena en las dependencias en las que aplique, debe existir evidencia objetiva de su participación en la reunión del mes informado, específicamente un registro de asistencia junto con el acta respectiva.",
+                "frecuencia": "mensual",
+                "criterios": "Registro de asistencia, junto con acta respectiva.",
+                "requiere_evidencia": 1
+            },
+            {
+                "codigo": "1.3",
+                "descripcion": "La actividad corresponde a la presentación de resultados correspondientes al programa +seguridad contratistas al administrador de contrato Abastible y Equipo de Integridad Operacional. La presentación se deberá realizar bajo el formato establecido por Abastible.",
+                "frecuencia": "mensual",
+                "criterios": "Registro de Asistencia",
+                "requiere_evidencia": 1
+            }
+        ]
+    },
+    {
+        "numero": "2",
+        "nombre": "Evaluación del riesgo y Gestión del riesgo",
+        "actividades": [
+            {
+                "codigo": "2.1",
+                "descripcion": "Realizar una sesión de seguridad, desarrollando un análisis de riesgos cualitativo (AST) de una tarea que se desprenda de la matriz de riesgos del proceso de distribución envasado, generada por Abastible. El análisis debe ser compartido mediante una sesión de seguridad con el personal del segmento al cual pertenezca. Esta actividad se dará por realizada cuando exista un análisis de riesgos (AST) y esta información se encuentre difundida al personal mediante una sesión de seguridad, por tanto, el criterio de cumplimiento considera 02 documentos: AST y registro de asistencia.",
+                "frecuencia": "mensual",
+                "criterios": "Análisis de riesgos (AST) elaborado para difusión y registro de asistencia.",
+                "requiere_evidencia": 1
+            },
+            {
+                "codigo": "2.2",
+                "descripcion": "Realizar implementación, ejecución, seguimiento y control de actividades asociadas a los siguientes protocolos:\\na) PREXOR;\\nb) TMERT;\\nc) MMC;\\nd) Radiación UV;\\ne) Psicosocial;\\nSeguimiento mediante planilla proporcionada por Abastible S.A. El criterio de cumplimiento corresponderá al 90% de las tareas planificadas mensualmente por la empresa contratista, esto según planificación que debe ser abordada por cada empresa.",
+                "frecuencia": "mensual",
+                "criterios": "Registros generales de implementación",
+                "requiere_evidencia": 1
+            }
+        ]
+    },
+    {
+        "numero": "3",
+        "nombre": "Competencias y capacitación del personal",
+        "actividades": [
+            {
+                "codigo": "3.1",
+                "descripcion": "Consiste en realizar la inducción de todo trabajador nuevo, reasignación de cargo, o que haya estado un periodo extenso sin realizar las labores para las cuales fue contratado (ej. Licencia médica), o que haya reingresado posterior a un accidente. Para dar cumplimiento a este ítem se requiere un registro individualizado de capacitación con datos mínimos tales como: nombre, rut, cargo que desarrollará, empresa por la cual realiza la inducción, fecha, hora inicio, hora de término. ODI firmada por trabajador.",
+                "frecuencia": "mensual",
+                "criterios": "Registro individualizado de capacitación",
+                "requiere_evidencia": 1
+            },
+            {
+                "codigo": "3.2",
+                "descripcion": "Ejecución de las capacitaciones de acuerdo a plan anual de capacitación que se haya asignado al segmento de distribución envasado, ya sea en plataforma ACHS, en AULA (plataforma disponibilizada por Abastible), o las proporcionadas en forma directa por la jefatura o línea de supervisión. Para dar cumplimiento a este ítem se debe mantener disponible el registro de asistencia, diploma de la plataforma a la cual se acceda.",
+                "frecuencia": "mensual",
+                "criterios": "Registro de Asistencia",
+                "requiere_evidencia": 1
+            }
+        ]
+    },
+    {
+        "numero": "6 y 7",
+        "nombre": "Operaciones e Integridad Mecánica",
+        "actividades": [
+            {
+                "codigo": "4.1",
+                "descripcion": "Aplicación de la lista de verificación de cumplimiento de MMC y EPP. Consiste en aplicar checklist para verificar el correcto MMC de acuerdo a los procedimientos establecidos por Abastible y el de cada empresa colaboradora.",
+                "frecuencia": "mensual",
+                "criterios": "Check list OBS/INSP MMC",
+                "requiere_evidencia": 1
+            },
+            {
+                "codigo": "4.2",
+                "descripcion": "Verificación de correcta aplicación de Check List salida camión envasado. Verificación de la existencia del check list de Distribución envasado, previo a la salida, es 01 check por cada camión que sale a ruta. Toda observación que se desprenda del check debe estar contenida en una tarjeta amarilla. Realizar revisión aleatoria de los checklist de salida de camión. Subir como evidencia en isotools. Subir hallazgo a isotools, con responsable y fecha.",
+                "frecuencia": "mensual",
+                "criterios": "Check list de salida camión envasado",
+                "requiere_evidencia": 1
+            },
+            {
+                "codigo": "4.3",
+                "descripcion": "Gestión de cierre de tarjetas. Gestionar cierre de tarjetas levantadas a la EECC y por la empresa contratista. Revisión Mensual de cierre de tarjetas.",
+                "frecuencia": "mensual",
+                "criterios": "Check list salida camión granel",
+                "requiere_evidencia": 1
+            }
+        ]
+    },
+    {
+        "numero": "9",
+        "nombre": "SERVICIOS DE TERCEROS",
+        "actividades": [
+            {
+                "codigo": "5.1",
+                "descripcion": "Garantizar el 100% de Acreditación para EECC y trabajadores que prestan servicio activo en Abastible. Consiste en evidenciar estatus de acreditación desde plataforma de control, los casos que no cumplan el 100% deben estar gestionadas y en seguimiento para dar conformidad a esta actividad.",
+                "frecuencia": "mensual",
+                "criterios": "Plataforma OVAL",
+                "requiere_evidencia": 1
+            },
+            {
+                "codigo": "5.2",
+                "descripcion": "Garantizar el 100% en Verificación laboral [pago de obligaciones laborales y previsionales]. Consiste en evidenciar el estatus de cumplimiento desde la plataforma de control, los casos que no cumplan con el 100% deberán gestionar las contingencias para dar conformidad a esta actividad.",
+                "frecuencia": "mensual",
+                "criterios": "Plataforma OVAL",
+                "requiere_evidencia": 1
+            },
+            {
+                "codigo": "5.3",
+                "descripcion": "Realizar reunión de accountability mensual, presentando Resumen estadístico y evidencia de cada actividad ejecutada. Consiste en sesionar mensualmente con el administrador de contrato, para evaluación periódica de desempeño.",
+                "frecuencia": "mensual",
+                "criterios": "Registro de Asistencia",
+                "requiere_evidencia": 1
+            }
+        ]
+    },
+    {
+        "numero": "10",
+        "nombre": "Investigación de accidentes",
+        "actividades": [
+            {
+                "codigo": "6.1",
+                "descripcion": "E-10 Envío de informe de investigación en plazo. Consiste en el envío del informe de investigación de acuerdo a la metodología y los plazos definidos por Abastible. En caso de que Abastible lo solicite, el contratista debe participar en reunión para revisión de la investigación. (reporte preliminar, simplificado, ICAM)",
+                "frecuencia": "cuando_aplique",
+                "criterios": "Correo envío de investigación",
+                "requiere_evidencia": 1
+            },
+            {
+                "codigo": "6.2",
+                "descripcion": "E-10 Verificación del cierre de las acciones correctivas derivadas de informes de investigación de accidentes. Verificar el estado de cierre de las acciones correctivas derivadas de informes de investigación de accidentes y hacer seguimiento al cierre. Esta información se lleva mediante un indicador llamado \"IMC\" índice de medidas correctivas, indicador que debe estar siempre al 100%.",
+                "frecuencia": "mensual",
+                "criterios": "Registro de acciones correctivas, según lo indicado en el plan de acción.",
+                "requiere_evidencia": 1
+            }
+        ]
+    },
+    {
+        "numero": "12",
+        "nombre": "Evaluación y Mejora de la Integridad de las Operaciones",
+        "actividades": [
+            {
+                "codigo": "7.1",
+                "descripcion": "E-12 Nro. De Incidentes no registrables: Nro de Incidentes no registrables ocurridos en el mes. (Seguridad de personas que no afecte indicadores). En esta actividad se debe hacer entrega del RESUMEN ESTADÍSTICO para dar un CUMPLE a la actividad.",
+                "frecuencia": "mensual",
+                "criterios": "Resumen Estadístico",
+                "requiere_evidencia": 1
+            },
+            {
+                "codigo": "7.2",
+                "descripcion": "E-12 Nro. De Incidentes registrables: Nro de Incidentes registrables ocurridos en el mes (asociados a seguridad de personas, indicadores a los cuales se relaciona IFF - IFT - IGT). En esta actividad se debe hacer entrega del RESUMEN ESTADÍSTICO para dar un CUMPLE a la actividad.",
+                "frecuencia": "mensual",
+                "criterios": "Resumen Estadístico",
+                "requiere_evidencia": 1
+            }
+        ]
+    }
+];
+
+module.exports = envasadoProgramData;
