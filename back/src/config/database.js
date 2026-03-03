388267f5ff492fde9d179f6623ab22a3b5231b67
pablosolisdalencon <inntek1.5dev@gmail.com>
2026-03-03 18:03:27 -0300

    Update database.js

--- back/src/config/database.js
+++ back/src/config/database.js
@@ -1,14 +1,16 @@
-// Carga dotenv para que funcione localmente o en scripts de CLI
 require('dotenv').config();
 
+const config = {
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_ROOT_PASSWORD, // Asegúrate de que este nombre sea igual al del .env
+  database: process.env.DB_NAME || 'abastible_oiem_preprod',
+  host: process.env.DB_HOST || 'db',
+  dialect: 'mysql',
+  logging: false,
+};
+
 module.exports = {
-  // Asegúrate de que el nombre coincida con lo que pasas en el flag --env
-  preproduction: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_ROOT_PASSWORD, // <--- Verifica que esta variable sea igual al .env
-    database: process.env.DB_NAME || 'abastible_oiem_preprod',
-    host: process.env.DB_HOST || 'db',
-    dialect: 'mysql', // <--- Esto soluciona el error del Dialect
-    logging: false
-  }
+  development: config,
+  preproduction: config,
+  production: config
 };
